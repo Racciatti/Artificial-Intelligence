c3c92d16719c57a176afeb4aa323f433c747f0ee
matheusvirissimo <matheus.virissimo@unesp.br>
2025-08-09 17:56:04 -0300

    feat: classe para o problema com grafos

--- searchAlgorithms/matheus/DFS.py
+++ searchAlgorithms/matheus/DFS.py
@@ -3,6 +3,16 @@
 from multiprocessing import util
 
 def depth_first_search(problem):
+    """
+    Realiza a busca em profundidade no problema fornecido.
+
+    ------
+    Params:
+        problem: O problema a ser resolvido
+
+    Returns:
+        None: caso a função não encontre uma solução
+    """
     node = get_start_node(problem)
 
     fronteira = util.Stack()
@@ -25,8 +35,63 @@ def depth_first_search(problem):
             return node['PATH']
         
         for sucessor in problem.expand(node['STATE']):
-            no_filho = get_child_node(sucessor, node)
-            fronteira.push(no_filho)
+            node_filho = get_child_node(sucessor, node)
+            fronteira.push(node_filho)
 
     # se dentro do while não encontramos nada, retornamos vazio 
     return []
+
+class ProblemaComGrafo:
+    def __init__(self, grafo, estado_inicial, estado_objetivo):
+        """
+        Inicializa o problema com um grafo, estado inicial e estado objetivo.
+
+        -------
+        Params:
+            grafo: O grafo que representa o problema
+            estado_inicial: O estado inicial do problema
+            estado_objetivo: O estado objetivo do problema
+
+        Returns:
+            None
+        """
+        self.grafo = grafo 
+        self.estado_inicial = estado_inicial 
+        self.estado_objetivo = estado_objetivo 
+
+    # Retorna o estado inicial do problema
+    def get_start_node(self):
+        """
+        Retorna o estado inicial do problema
+
+        -------
+        Returns:
+            estado_inicial: O estado inicial do problema
+        """
+        return self.estado_inicial
+
+    def is_goal_state(self, estado):
+        """
+        Verifica se o estado ATUAL é o estado objetivo do problema
+
+        -------
+        Params:
+            estado: O estado a ser verificado
+
+        Returns:
+            bool: True se o estado for o objetivo, False caso contrário
+        """
+        return estado == self.estado_objetivo
+
+    def expand(self, estado):
+        """
+        Faz a expansão do nó, fornecendo seus nós-filhos
+
+        -------
+        Params:
+            estado: O nó a ser expandido
+
+        Returns:
+            list: Uma lista de nós-filhos resultantes da expansão
+        """
+        return self.grafo.get(estado, [])
