e7b4849f72dbe1700c1965b3bb3b023328862480
matheusvirissimo <matheus.virissimo@unesp.br>
2025-08-30 19:58:13 -0300

    docs: docstrings de mais funções

--- qLearningAlgorithms/matheus/qtable_taxi.py
+++ qLearningAlgorithms/matheus/qtable_taxi.py
@@ -25,15 +25,19 @@ def q_learning(alpha, gamma, epsilon, epsilon_decay, min_epsilon, num_episodios,
     q_tabela: np.ndarray
         Tabela que armazena os valores de ação para cada estado.
 
+    Returns
+    -------
+    q_tabela: np.ndarray
+        A tabela Q atualizada após o treinamento.
     """
     for episodio in range(num_episodios):
         estado, _ = env.reset() # reset o ambiente e pega o estado inicial
 
-        feito = False 
+        concluido = False 
 
         for passo in range(max_passos):
             acao = escolher_acao(estado)
-            proximo_estado, recompensa, feito, truncamento, _ = env.step(acao) # retorna o próximo estado, a recompensa e se o episódio terminou
+            proximo_estado, recompensa, concluido, truncamento, _ = env.step(acao) # retorna o próximo estado, a recompensa e se o episódio terminou
 
             valor_antigo = q_tabela[estado, acao] # pega o valor antigo da q-tabela e é um q-estado
             proximo_valor = np.max(q_tabela[proximo_estado, :]) # qual a melhor ação disponível no próximo estado
@@ -43,7 +47,7 @@ def q_learning(alpha, gamma, epsilon, epsilon_decay, min_epsilon, num_episodios,
 
             estado = proximo_estado
 
-            if feito or truncamento: 
+            if concluido or truncamento: 
                 break
 
         epsilon = max(min_epsilon, epsilon * epsilon_decay) ## decai o epsilon
@@ -52,36 +56,61 @@ def q_learning(alpha, gamma, epsilon, epsilon_decay, min_epsilon, num_episodios,
         if (episodio + 1) % 20 == 0:
             print(f"Episódio {episodio + 1}/{num_episodios} concluído. Quantidade de passos: {passo + 1}. Recompensa: {recompensa}. Epsilon: {epsilon:.4f}")
 
-    print("Treinamento concluído! Testando o agente treinado...")
+    print("Treinamento concluído")
 
     return q_tabela
 
 def escolher_acao(estado):
+    """
+    Função para escolher a próxima ação baseado no estado
+
+    Parameters 
+    ----------
+    estado: int 
+        O estado s atual do ambiente
+    
+    Returns 
+    -------
+    acao_aleatoria: int
+        A ação aleatória escolhida por estar abaixo de epsilo n
+    melhor_acao: int 
+        Retorna o index da melhor ação daquele estado disponível na nossa q-tabela
+
+    """
     if random.uniform(0, 1) < epsilon: ## Vamos tomar uma decisão aleatória 
-        return env.action_space.sample() # pega uma ação do espaço aleatório e retorna
+        acao_aleatoria = env.action_space.sample()
+        return acao_aleatoria 
     else:
-        return np.argmax(q_tabela[estado, :]) # retorna o index da melhor ação segundo a q-tabela
+        melhor_acao = np.argmax(q_tabela[estado, :])
+        return melhor_acao 
 
 ## Criando o jogo
 env = gym.make("Taxi-v3")
 
+## Parâmetros que iremos utilizar
 alpha = 0.9 ## learning rate
 gamma = 0.9 ## desconto
 epsilon = 1 ## exploração rate, determina o nosso randomness e maximiza o aprendizado. 1 tudo é random e 0 o q-learning é desnecessário
-epsilon_decay = 0.9995 ## Diminuir a exploração ao longo do tempo
-min_epsilon = 0.01 ## Limite mínimo para o epsilon e continuar com ações aleatórias 
-num_episodios = 10000 ## quantas vezes o agente irá jogar o jogo (reduzido para teste) 
+epsilon_decay = 0.9995 ## Diminuir a exploração ao longo do tempo e depender mais das experiências do agente com o q-learning
+min_epsilon = 0.01 ## Limite mínimo para o epsilon e ainda continuar com ações aleatórias 
+num_episodios = 1000 ## quantas vezes o agente irá jogar o jogo 
 max_passos = 100 ## número máximo de passos por episódio
 
 # 5x5 grid -> 25 posições para o taxi * 5 locais onde o passageiro pode estar * 4 locais do hotel = 500 estados possíveis
 # a tabela começa com 0 porque o agente não experimentou nada ainda
-q_tabela = np.zeros((env.observation_space.n, env.action_space.n)) ## temos um espaço observável e quantas ações podemos tomar por estado
+q_tabela_inicial = np.zeros((env.observation_space.n, env.action_space.n)) ## temos um espaço observável e quantas ações podemos tomar por estado
 
 
-q_tabela = q_learning(alpha, gamma, epsilon, epsilon_decay, min_epsilon, num_episodios,max_passos, q_tabela)
+## Criando o jogo VISUAL
+q_tabela = q_learning(alpha, gamma, epsilon, epsilon_decay, min_epsilon, num_episodios, max_passos, q_tabela_inicial)
 
-## Criando o jogo
 env = gym.make("Taxi-v3", render_mode="human")
+print("\n\n*** Taxi-Driver****\n\n" \
+"Esse jogo simula um táxi em uma grade 5x5 onde o objetivo é pegar e deixar passageiros em um hotel.\n"
+"A cada episódio, o táxi deve aprender a navegar pela grade e otimizar suas rotas.\n"
+"O táxi pode se mover para o norte, sul, leste ou oeste, pegar passageiros e deixá-los em seus destinos.\n"
+"Tanto a localização dos passageiros, do hotel e do taxi são randomizadas a cada episódio.\n")
+
 
 for episodio in range(3):  # Reduzido para apenas 3 episódios de teste
     estado, _ = env.reset() 
