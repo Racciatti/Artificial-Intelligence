f0a657e0e659a071b7d74736411b2dc20d40ff19
Racciatti <enzonracciatti@gmail.com>
2025-08-31 12:40:58 -0300

    add maze runner and dependencies

---
+++ qLearningAlgorithms/racciatti/main.py
@@ -0,0 +1,35 @@
+from games.maze import MazeEnv
+from agents.mazeAgent import MazeAgent
+from runners import MazeRunner
+import pandas as pd
+from matplotlib import pyplot as plt
+
+# Defining test batch name
+TEST_NAME = 'test'
+
+# Defining which hyperparameters we want to test
+learning_rates = [0.001, 0.01, 0.1, 0.3, 0.8]
+epsilons = [0.3]
+
+# Defining how many runs we want to do
+N_RUNS = 1
+
+# Creating game
+maze = MazeEnv(n=12,num_obstacles=20)
+
+# Creating the runner
+runner = MazeRunner(maze)
+
+# Creating agents and adding them to the runner
+agents = []
+for learning_rate in learning_rates:
+    for epsilon in epsilons:
+        runner.add_agent(MazeAgent(alpha=learning_rate, gamma=1-learning_rate, epsilon=epsilon, game=maze))
+
+# For each run
+for i in range(N_RUNS):
+    # Run the algorithm
+    runner.run(200,5,TEST_NAME+str(i+1))
+
+    # Save visualizations of the stored results
+    runner.view(TEST_NAME+str(i+1))
