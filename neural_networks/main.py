418f914ba813cde0f06013d728c2934f19060fc6
Racciatti <enzonracciatti@gmail.com>
2025-10-28 18:16:54 -0300

    add xor examples

--- neural_networks/main.py
+++ neural_networks/main.py
@@ -10,6 +10,13 @@ square = lambda x: x**2
 der_relu = lambda x : 1 if x > 0 else 0
 
 
+xor_examples = [
+    [0,0,0],
+    [0,1,1],
+    [1,0,1],
+    [1,1,0]
+    ]
+
 neuralNet = NeuralNetwork(neurons_per_layer=[3,2,1], activation_function=identity,verbose=True)
 
 neuralNet.feed([i for i in range(3)])
